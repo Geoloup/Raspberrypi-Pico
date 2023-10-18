d2d72e4c75e8bc48da0828f7a27726ad9f4efe70
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-18 11:43:30 -0400

    adding new code

---
+++ serials/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', event => {
+    async() => {
+      let device
+      
+      try {
+        device = await navigator.usb.requestDevice()
+  
+        console.log('open')
+        await device.open()
+        console.log('opened:', device)
+      } catch (error) {
+        console.log(error)
+      }
+        await device.close()
+    }
+  })
