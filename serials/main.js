edef4186d0dfcaa6aac481062b4a0f4326e92e5e
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-18 14:49:28 -0400

    document en forme

--- serials/main.js
+++ serials/main.js
@@ -1,31 +1,31 @@
 async function serialMain() {
-var ports = await navigator.serial.getPorts();
-if (ports == []) {
-    var port = await navigator.serial.requestPort();
-}
-await port.open({ baudRate: 9600 /* pick your baud rate */ });
-
-if (port.readable) {
-    const reader = port.readable.getReader();
-    try {
-        while (true) {
-            const { value, done } = await reader.read();
-            if (done) {
-                // |reader| has been canceled.
-                break;
-            }
-            // Do something with |value|…
-        }
-    } catch (error) {
-        // Handle |error|…
-    } finally {
-        reader.releaseLock();
+    var ports = await navigator.serial.getPorts();
+    if (ports == []) {
+        var port = await navigator.serial.requestPort();
     }
-    const encoder = new TextEncoder();
-    const writer = port.writable.getWriter();
-    save()
-    await writer.write(encoder.encode(loadLastReturn()));
-    writer.releaseLock();
-}
+    await port.open({ baudRate: 9600 /* pick your baud rate */ });
+
+    if (port.readable) {
+        const reader = port.readable.getReader();
+        try {
+            while (true) {
+                const { value, done } = await reader.read();
+                if (done) {
+                    // |reader| has been canceled.
+                    break;
+                }
+                // Do something with |value|…
+            }
+        } catch (error) {
+            // Handle |error|…
+        } finally {
+            reader.releaseLock();
+        }
+        const encoder = new TextEncoder();
+        const writer = port.writable.getWriter();
+        save()
+        await writer.write(encoder.encode(loadLastReturn()));
+        writer.releaseLock();
+    }
 }
 serialMain()
