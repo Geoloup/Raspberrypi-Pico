6eb7ba7dca80b478b6e93b32ab54076b9ec569ab
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-25 16:24:39 -0400

    Fixed bug

---
+++ config/compile.js
@@ -0,0 +1,3 @@
+function compile(code) {
+    return code.replaceAll()
+}
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 script("config/log.js")
 script("config/save.js")
-Script("serials/main.js")
+script("serials/main.js")
 script("config/main.js")
