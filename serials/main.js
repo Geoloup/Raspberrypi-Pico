ec6e644e0ca0eb2ca95384b27b691a5083bd7c59
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-24 20:44:49 -0400

    Adding main feature

--- serials/main.js
+++ serials/main.js
@@ -1,13 +1,1 @@
-async function serialMain() {
-    var port = await navigator.serial.requestPort();
-    await port.open({ baudRate: 9600 /* pick your baud rate */ });
-    window.pico = port
-
-    if (port.readable) {
-        const encoder = new TextEncoder();
-        const writer = port.writable.getWriter();
-        save()
-        await writer.write(encoder.encode(loadLastReturn()));
-        writer.releaseLock();
-    }
-}
+script("serials/controller.js")
