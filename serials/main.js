5851e32d3727dd55ab640cee662ea8e2fca4b98e
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-25 20:41:54 -0400

    Add fix bug

--- serials/main.js
+++ serials/main.js
@@ -2,6 +2,7 @@ document.getElementById("RunButton").addEventListener("click",(event) => {
     if (window.connect) {
         console.log("Hi")
         save()
+        SerialWriteCustom(3)
         serialWrite(compile(loadLastReturn()))
         SerialWriteCustom(13)
     }
