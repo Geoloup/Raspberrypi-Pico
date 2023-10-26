5eaf55b73829b431339ef9618a722e982c612635
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-26 09:31:34 -0400

    Update the sys and add console visiblyty

--- serials/main.js
+++ serials/main.js
@@ -7,3 +7,10 @@ document.getElementById("RunButton").addEventListener("click",(event) => {
         SerialWriteCustom(13)
     }
 })
+document.getElementById("ConsoleButton").addEventListener("click",(event) => {
+    if (window.connect) {
+        console.log("Disaplying console")
+        document.getElementById("term").style.display = "block"
+        document.getElementById("term").style.position = "fixed"
+    }
+})
