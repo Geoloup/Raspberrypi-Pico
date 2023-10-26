ab026cc1ef0c975d9e0427bd97b3e87efc9ae98b
franck403 <79372051+franck403@users.noreply.github.com>
2023-10-26 09:33:01 -0400

    Changed to toggle console style

--- serials/main.js
+++ serials/main.js
@@ -10,7 +10,11 @@ document.getElementById("RunButton").addEventListener("click",(event) => {
 document.getElementById("ConsoleButton").addEventListener("click",(event) => {
     if (window.connect) {
         console.log("Disaplying console")
-        document.getElementById("term").style.display = "block"
+        if (document.getElementById("term").style.display == "block") {
+            document.getElementById("term").style.display = "none"
+        } else {
+            document.getElementById("term").style.display = "block"
+        }
         document.getElementById("term").style.position = "fixed"
     }
 })
